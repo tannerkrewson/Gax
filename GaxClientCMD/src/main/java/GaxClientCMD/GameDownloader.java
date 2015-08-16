507cede00b8fc6a27a4f04a92c03f7ced1aff3bc
TankTan38 <iamtanktan@gmail.com>
2015-08-16 15:44:01 -0500

    Changed HTTP download method with resumable and progress support

--- GaxClientCMD/src/main/java/GaxClientCMD/GameDownloader.java
+++ GaxClientCMD/src/main/java/GaxClientCMD/GameDownloader.java
@@ -1,32 +1,75 @@
 package GaxClientCMD;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.JSONObject;
 import org.zeroturnaround.zip.ZipUtil;
 
 public class GameDownloader {
 
-    public boolean downloadGame(int gid) {
-
-        //first tell the server what we about to throw down
-        JSONObject jo = GaxClient.sendCommand("download " + gid);
-        System.out.println(jo.toString());
-        //check if the game exists and if the server is ready to send to us
-        boolean success = jo.getBoolean("success");
-        if (!success) {
-            System.out.println("Download game failed: Error code " + jo.getInt("reason"));
-            return false;
-        }
-        String downloadURL = jo.getString("url");
-
-        //installation directory
-        String sdir = GaxClient.cm.ConfigDir + "GaxGames/" + gid + "/";
-        String sfile = gid + ".zip";
+    public boolean downloadGame(int gid){
 
         try {
+
+            //first tell the server what we about to throw down
+            JSONObject jo = GaxClient.sendCommand("download " + gid);
+            System.out.println(jo.toString());
+            //check if the game exists and if the server is ready to send to us
+            boolean success = jo.getBoolean("success");
+            if (!success) {
+            System.out.println("Download game failed: Error code " + jo.getInt("reason"));
+            return false;
+            }
+            URL downloadURL = new URL(jo.getString("url"));
+
+            //URL downloadURL = new URL("http://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-solid-color-background.jpg");
+            
+            //installation directory
+            String sdir = GaxClient.cm.ConfigDir + "GaxGames/" + gid + "/";
+            String sfile = gid + ".zip";
+            
+            File outputFile = new File(sdir + sfile);
+            //this will create all of the required folders if need be
+            (new File(sdir)).mkdirs();
+            //this will not create a new file if one is already there
+            outputFile.createNewFile();
+            
+            HttpURLConnection connection = (HttpURLConnection) downloadURL.openConnection();
+            long downloaded = 0;
+            //checks if it was paused before or not
+            
+            if (outputFile.exists()) {
+                downloaded = (long) outputFile.length();
+                connection.setRequestProperty("Range", "bytes=" + (outputFile.length()) + "-");
+            } else {
+                connection.setRequestProperty("Range", "bytes=" + downloaded + "-");
+            }
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            long fileSize = connection.getContentLengthLong();
+            BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
+            FileOutputStream fos = (downloaded == 0) ? new FileOutputStream(outputFile) : new FileOutputStream(outputFile, true);
+            BufferedOutputStream bout = new BufferedOutputStream(fos, 1024);
+            byte[] data = new byte[1024];
+            int x = 0;
+            int percent = 1;
+            while ((x = in.read(data)) > 0) {
+                bout.write(data, 0, x);
+                downloaded += x;
+                if (downloaded >= (fileSize / 100) * percent){
+                    System.out.println(percent + "% complete");
+                    percent++;
+                }
+            }
+            in.close();
+            bout.close();
+            /*
+            try {
             File outputFile = new File(sdir + sfile);
             //this will create all of the required folders if need be
             (new File(sdir)).mkdirs();
@@ -36,8 +79,13 @@ public class GameDownloader {
             ReadableByteChannel rbc = Channels.newChannel(website.openStream());
             FileOutputStream fos = new FileOutputStream(outputFile);
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        } catch (Exception ex) {
+            } catch (Exception ex) {
             System.out.println("Failed to download url to file");
+            ex.printStackTrace();
+            return false;
+            }
+            return true;*/
+        } catch (IOException ex) {
             ex.printStackTrace();
             return false;
         }
